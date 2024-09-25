05853aa02e8f25df7e9d7433a3a2aa1d82a0ea76
Unais-12 <unaisbinfaheem16@gmail.com>
2024-09-25 17:24:15 +0500

    Changes

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: chmod +x ./install_odbc.sh && ./install_odbc.sh
 web: gunicorn app:app --workers=2
--- install_odbc.sh
+++ install_odbc.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
 
-# Add Microsoft repository key and package
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
-# Update package lists
-apt-get update
-
-# Install the msodbcsql17 driver
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
