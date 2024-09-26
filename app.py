ac0c6762e682e79c79cb82cc6b1d3d3fbae01673
Unais-12 <unaisbinfaheem16@gmail.com>
2024-09-26 23:31:29 +0500

    Destructive

--- app.py
+++ app.py
@@ -18,8 +18,6 @@ if __name__ == "__main__":
 
 app.config["SESSION_TYPE"] = 'filesystem'
 app.config["SESSION_PERMANENT"] = False
-app.config['SESSION_COOKIE_NAME'] = 'my_custom_session'
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
 Session(app)
 
 
