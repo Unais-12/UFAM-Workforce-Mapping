83f7640f27f31ed18b6fef5114b580e5da2345c9
Unais-12 <unaisbinfaheem16@gmail.com>
2024-09-26 22:19:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ import uuid
 
 
 app = Flask(__name__)
+
+@app.route('/health')
+def health_check():
+    return "Healthy", 200
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
 
