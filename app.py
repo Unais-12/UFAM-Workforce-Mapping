38117d1bdd0f191b61a9a62e4b03008befb192fc
Unais-12 <unaisbinfaheem16@gmail.com>
2024-10-18 21:35:33 +0500

    CHANGES GANG

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ app.config['SESSION_COOKIE_NAME'] = 'my_custom_session'
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
 app.config['SESSION_USE_SIGNER'] = True
 app.config['SESSION_KEY_PREFIX'] = 'hyphen_survey:'
-app.config['SENDGRID_API_KEY'] = os.getenv('SENDGRID_API_KEY')
+app.config['SENDGRID_API_KEY'] = os.getenv('SENDGRID_API_KEY', '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')
 
 serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 Session(app)
@@ -72,7 +72,7 @@ def forgot_password():
         )
 
         try:
-            sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
+            sg = SendGridAPIClient(app.config['SENDGRID_API_KEY'])
             response = sg.send(message)
             print(response.status_code)
             print(response.body)
