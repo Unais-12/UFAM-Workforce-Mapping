1c27a45d1f2344d8f4b4c68e7ef1d1eeec2e8d3d
Unais-12 <unaisbinfaheem16@gmail.com>
2024-10-19 14:23:53 +0500

    hopefully the fix

--- app.py
+++ app.py
@@ -110,9 +110,10 @@ def reset_password(token):
         conn.commit()
         conn.close
         
+        flash('Your password has been successfully reset.', 'success')
         return redirect('/login')
     
-    return render_template('reset_password.html')
+    return render_template('reset_password.html', token=token)
 
 
 
